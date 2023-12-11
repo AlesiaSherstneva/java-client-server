3c0f57a0f25c5f0c0f7e9b5316c3a50fb6a85bab
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-12 02:15:20 +0300

    Send symbol '@' to client

--- src/Server.java
+++ src/Server.java
@@ -2,10 +2,14 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+@SuppressWarnings("DanglingJavadoc")
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8000);
         Socket clientSocket = serverSocket.accept();
+
+        /** send symbol '@' to client*/
+        // clientSocket.getOutputStream().write(64);
 
         clientSocket.close();
         serverSocket.close();
