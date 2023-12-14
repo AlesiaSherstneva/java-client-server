9f2e24ee369f22b84955b45cb2397aa9b4b530ba
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-14 11:01:17 +0300

    Read one symbol from server

--- src/Client.java
+++ src/Client.java
@@ -4,5 +4,9 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) throws IOException {
         Socket clientSocket = new Socket("127.0.0.1", 8000);
+
+        System.out.println(clientSocket.getInputStream().read());
+
+        clientSocket.close();
     }
 }
