59cca12446a0dd2c07c87aa1c00a469972a38a12
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-14 11:16:55 +0300

    Change the message on the server

--- src/Server.java
+++ src/Server.java
@@ -5,20 +5,18 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        int client = 0;
+        int count = 0;
         ServerSocket serverSocket = new ServerSocket(8000);
 
         while (true) {
             Socket clientSocket = serverSocket.accept();
-            System.out.println("Client accepted " + ++client);
+            System.out.println("Client accepted " + ++count);
 
             OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
-            writer.write(
-                    """
-                            HTTP/1.0 200 OK\r
-                            Content-type: text/html\r
-                            \r
-                            <h1>Java</h1>""");
+            writer.write(//"HTTP/1.0 200 OK\r\n" +
+                    //"Content-type: text/html\r\n" +
+                    //"\r\n" +
+                    "You are client №" + count + "\n");
             writer.flush();
 
             writer.close();
