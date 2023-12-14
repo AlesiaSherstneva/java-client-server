e5a2886d80348f6d04bf43e249d261655a151ffd
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-14 10:22:57 +0300

    Create class for client

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket clientSocket = new Socket("127.0.0.1", 8000);
+    }
+}
