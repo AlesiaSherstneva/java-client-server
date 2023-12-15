2fe4b3de174aeabbdcbaeed1cfaf7254071b2398
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-15 15:13:57 +0300

    Send request from client to server

--- src/Client.java
+++ src/Client.java
@@ -1,16 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
         Socket clientSocket = new Socket("127.0.0.1", 8000);
 
+        OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
         BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        writer.write("Get me some information");
+        writer.flush();
+
         String message = reader.readLine();
         System.out.println(message);
 
+        writer.close();
         reader.close();
         clientSocket.close();
     }
