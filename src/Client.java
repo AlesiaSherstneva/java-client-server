f85df944294635d13d6debd35929a0481ec485ef
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-16 23:51:39 +0300

    Make the game: client tries to guess the server's number

--- src/Client.java
+++ src/Client.java
@@ -5,19 +5,46 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
+@SuppressWarnings("BusyWait")
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Socket clientSocket = new Socket("127.0.0.1", 8000);
+        System.out.println("Client started");
 
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
         BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        writer.write("Get me some information");
+        int lowestNum = 1, highestNum = 1000, middleNum = (highestNum - lowestNum) / 2;
+
+        System.out.println(reader.readLine());
+
+        Thread.sleep(3000);
+        System.out.println(middleNum);
+        writer.write(String.valueOf(middleNum));
         writer.newLine();
         writer.flush();
 
-        String response = reader.readLine();
-        System.out.println(response);
+
+        while (true) {
+            String response = reader.readLine();
+            if (response.equals("less")) {
+                highestNum = middleNum;
+            } else if (response.equals("more")) {
+                lowestNum = middleNum;
+            } else {
+                Thread.sleep(2000);
+                System.out.println("I've won!!! WooHoo!!!");
+                break;
+            }
+
+            Thread.sleep(3000);
+            middleNum = (highestNum + lowestNum) / 2;
+            System.out.println(middleNum);
+
+            writer.write(String.valueOf(middleNum));
+            writer.newLine();
+            writer.flush();
+        }
 
         reader.close();
         writer.close();
--- src/Server.java
+++ src/Server.java
@@ -5,29 +5,47 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 public class Server {
     public static void main(String[] args) throws IOException, InterruptedException {
-        int count = 0;
         ServerSocket serverSocket = new ServerSocket(8000);
         System.out.println("Server started");
 
-        while (true) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Client accepted " + ++count);
+        Socket clientSocket = serverSocket.accept();
 
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            String request = reader.readLine();
-            Thread.sleep(5000);
-            String response = "№" + count + ", your message length is " + request.length();
-            writer.write(response);
+        int serverNum = new Random().nextInt(1000) + 1;
+        System.out.println("My number is: " + serverNum);
+        writer.write("I thought of a number from 1 to 1000. Guess which one?");
+        writer.newLine();
+        writer.flush();
+        boolean wasGuessed = false;
+
+        while (!wasGuessed) {
+            Thread.sleep(3000);
+            String response = reader.readLine();
+            String request;
+            int tempNum = Integer.parseInt(response);
+            Thread.sleep(3000);
+            if (tempNum > serverNum) {
+                request = "less";
+            } else if (tempNum < serverNum) {
+                request = "more";
+            } else {
+                request = "Yes, you're right! It was the number: " + serverNum;
+                wasGuessed = true;
+            }
+            writer.write(request);
+            writer.newLine();
             writer.flush();
-
-            reader.close();
-            writer.close();
-            clientSocket.close();
+            System.out.println(request);
         }
+
+        reader.close();
+        writer.close();
+        clientSocket.close();
     }
 }
