7e0fcb4b26bd517f873e862b335dcf3201d0838e
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-21 05:46:33 +0300

    Create class for the phone

---
+++ app-02-socketor/src/Phone.java
@@ -0,0 +1,20 @@
+public class Phone {
+    public Phone(String port) {
+    }
+
+    public Phone(String ip, String port) {
+    }
+
+    public void accept() {
+    }
+
+    public String readLine() {
+        return null;
+    }
+
+    public void writeLine(String message) {
+    }
+
+    public void close() {
+    }
+}
