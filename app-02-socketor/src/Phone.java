632163fd6be2e5e0a4d3ccf235630d0942d611bf
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-22 11:27:17 +0300

    Create methods for reading and writing data

--- app-02-socketor/src/Phone.java
+++ app-02-socketor/src/Phone.java
@@ -54,10 +54,21 @@ public class Phone {
     }
 
     public String readLine() {
-        return null;
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void writeLine(String message) {
+        try {
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void close() {
