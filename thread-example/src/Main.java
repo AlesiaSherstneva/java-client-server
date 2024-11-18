66f9b392509d6401927366f59480bfcf48415280
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-11-18 21:22:22 +0300

    Change Worker class usage to lambda expression

--- thread-example/src/Main.java
+++ thread-example/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Worker worker = new Worker();
-
+        Runnable worker = () -> {
+            for (int j = 1; j <= 100; j++) {
+                System.out.println(j);
+            }
+        };
         for (int i = 0; i < 5; i++) {
             new Thread(worker).start();
         }
