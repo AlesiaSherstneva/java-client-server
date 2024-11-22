25d7efcfb423d8b78b914004eec99a9eef0dfef5
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-11-22 22:11:34 +0300

    Make multithreading Socketor and Phone

--- app-04-multisocketor/src/MultiSocketor.java
+++ app-04-multisocketor/src/MultiSocketor.java
@@ -26,6 +26,10 @@ public class MultiSocketor {
     private void runServer(String port, String threadsCount, String operation) {
         int threads = Integer.parseInt(threadsCount);
         MultiPhone[] phone = new MultiPhone[threads];
+        phone[0] = new MultiPhone(port);
+        for (int j = 1; j < threads; j++) {
+            phone[j] = new MultiPhone(phone[0]);
+        }
         System.out.printf("Started server with \"%s\" operation on %s port\n", operation, port);
 
         while (true) {
