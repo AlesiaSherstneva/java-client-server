9746a38d9cf5401d3a7a9af2e4e866c9ce5560a2
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-11-23 14:31:40 +0300

    Create runnable class for server phone

---
+++ app-04-multisocketor/src/ServerPhone.java
@@ -0,0 +1,12 @@
+public class ServerPhone implements Runnable {
+    private MultiPhone phone;
+
+    public ServerPhone(MultiPhone phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
