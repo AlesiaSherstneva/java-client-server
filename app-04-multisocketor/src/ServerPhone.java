b2f2a462f83278802559fc7c19e78c74c683d47a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-11-26 15:03:32 +0300

    Move calculate method to the server phone class

--- app-04-multisocketor/src/ServerPhone.java
+++ app-04-multisocketor/src/ServerPhone.java
@@ -1,12 +1,44 @@
 public class ServerPhone implements Runnable {
-    private MultiPhone phone;
+    private final MultiPhone phone;
+    private final String operation;
 
-    public ServerPhone(MultiPhone phone) {
+    public ServerPhone(MultiPhone phone, String operation) {
         this.phone = phone;
+        this.operation = operation;
     }
 
     @Override
+    @SuppressWarnings("BusyWait")
     public void run() {
+        while (true) {
+            phone.accept();
 
+            String a = phone.readLine();
+            String b = phone.readLine();
+            int result = calculate(operation, a, b);
+            String message = String.format("%s %s %s = %d\n", a, operation, b, result);
+
+            try {
+                Thread.sleep(7000);
+            } catch (InterruptedException e) {
+                break;
+            }
+
+            phone.writeLine(message);
+            System.out.printf(message);
+
+            phone.close();
+        }
+    }
+
+    private int calculate(String operation, String a, String b) {
+        int intA = Integer.parseInt(a), intB = Integer.parseInt(b);
+        return switch (operation) {
+            case "+" -> intA + intB;
+            case "-" -> intA - intB;
+            case "*" -> intA * intB;
+            case "/" -> intA / intB;
+            default -> throw new IllegalArgumentException("Wrong operation, choose one of \"+ - * /\"");
+        };
     }
 }
